b1782934c1e1204531f4c2bcdf568eaa142f1e02
Diogo Millnitz <diogo.millnitz@ad.britania.com.br>
2020-04-27 08:24:55 -0300

    before change

---
+++ Frontend/frontend/src/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Header(props) {
+
+    return (
+        <header>
+            <h1>
+                {props.title} 
+            </h1>
+        </header>
+
+    )
+
+}
